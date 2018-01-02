6fbeeaa54efcd848f927dbf54299ce39c7cf666f
Marius Olai Hansen <Marius.Hansen@bringdialog.no>
2018-01-02 23:50:18 +0100

    First commit 🎉

---
+++ README.md
@@ -0,0 +1,73 @@
+# Universal Parallax
+Easy parallax plugin using pure javascript. Cross browser support, including mobile platforms.
+[See demo here](https://github.com/marrio-h/UniversalParallax/demo)
+
+### Features
+- Easy setup
+- Pure JavaScript plugin - no jQuery needed
+- Adaptive height
+- Mobile phone support
+
+### Install
+`$ npm install universal-parallax -S`
+
+### Usage
+###### #1
+Include the script in your project.
+```html
+<script src="node_modules/universal-parallax/dist/universal-parallax.min.js"></script>
+```
+###### #2
+Include this CSS file in your project `<link href="node_modules/universal-parallax/dist/universal-parallax.min.css" rel="stylesheet">`
+
+Or add this CSS
+```css
+.parallax--container {
+	position: absolute;
+	clip: rect(0, auto, auto, 0);
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	z-index: -10;
+}
+
+.parallax {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	/* optional */
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+}
+```
+
+###### #3
+If `<section>` is your container, make the parallax element inside it.
+
+```html
+ <section>
+    <div class="parallax" data-parallax-image="path/to/your_image">
+ </section>
+```
+
+You can also use a class to define your `background-image` in CSS, instead of using `data-parallax-image=""`.
+
+###### #4
+Fire up the JS function.
+```js
+new universalParallax().init();
+```
+
+### Custom speed
+You can change the parallax speed.
+```js
+// Plugin customization
+new universalParallax().init({
+    speed: 6.0
+});
+```
+Note: `speed: 1.2` is the minimum value before the background image is fixed.
---
+++ dist/universal-parallax.js
@@ -0,0 +1,84 @@
+/**
+* @version 1.0.0
+* @author Marius Hansen <marius.o.hansen@gmail.com>
+* @license MIT
+* @description Easy parallax plugin using pure javascript. Cross browser support, including mobile platforms. Based on goodparallax
+* @copyright © Marius Hansen 2017
+*/
+
+
+var universalParallax = function() {
+
+	var up = function(parallax, speed) {
+
+		if(speed < 1.2){
+			speed = 0;
+		}
+
+		// Add scroll event listener
+		window.addEventListener("scroll", function() {
+			// apply effect to each element
+			for (var i = 0; parallax.length > i; i++) {
+
+				// get measurements
+				var parallaxContainer = parallax[i].parentElement;
+				var parentHeight = parallaxContainer.scrollHeight;
+				var parentTopOfElem = parallaxContainer.getBoundingClientRect().top;
+				var elemOffsetTop = (window.innerHeight - parentHeight) / 2;
+
+				// determine height
+				var bgHeight = parentHeight + ((elemOffsetTop - (elemOffsetTop / speed)) * 2);
+				parallax[i].style.height = bgHeight + 'px';
+
+				// set speed
+				var bgScroll = parentTopOfElem / speed;
+				parallax[i].style.top = bgScroll + 'px';
+
+			}
+		});
+	};
+
+
+	// Ready all elements
+	this.init = function(param) {
+		if(typeof param === 'undefined') {
+			param = {}
+		}
+
+		param = {
+			speed: typeof param.speed !== 'undefined' ? param.speed : 4,
+			className: typeof param.className !== 'undefined' ? param.className : 'parallax'
+		}
+		var parallax = document.getElementsByClassName(param.className);
+
+		for (var i = 0; parallax.length > i; i++) {
+			// make container div
+			var wrapper = document.createElement('div');
+			parallax[i].parentNode.insertBefore(wrapper, parallax[i]);
+			wrapper.appendChild(parallax[i]);
+			var parallaxContainer = parallax[i].parentElement;
+			parallaxContainer.className += 'parallax--container';
+			// parent elem need position: relative for effect to work - if not already defined, add it
+			if (window.getComputedStyle(parallaxContainer.parentElement, null).getPropertyValue('position') !== 'relative') {
+				this.style.position = 'relative';
+			}
+
+			var imgData = parallax[i].dataset.parallaxImage;
+			// add image to div if none is specified
+			if(typeof imgData !== 'undefined') {
+				parallax[i].style.backgroundImage = 'url('+imgData+')';
+				// if no other class than .parallax is specified, add CSS
+				if(parallax[i].classList.length === 1 && parallax[i].classList[0] === 'parallax') {
+					Object.assign(parallax[i].style,{
+						'background-repeat':'no-repeat',
+						'background-position':'center',
+						'background-size':'cover'
+					});
+				}
+			}
+		};
+
+		// when init completed, run function
+		up(parallax, param.speed);
+	};
+};
