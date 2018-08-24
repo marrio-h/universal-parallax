5f04e33131941a605d55520297aba8b4ed548f8c
Marius Olai Hansen <Marius.Hansen@bringdialog.no>
2018-08-24 14:45:04 +0200

    Revert "quality controlled code across all devices and improved js functions. Removed requestAnimationFrame cos it caused too much lag between layers."
    This reverts commit 039287e333d532defef7fce933526bb66fae6fb7.

--- dist/universal-parallax.js
+++ dist/universal-parallax.js
@@ -1,83 +1,78 @@
 'use strict';
 
 /**
-* @version 1.2.1
+* @version 1.2.0
 * @author Marius Hansen <marius.o.hansen@gmail.com>
 * @license MIT
 * @description Easy parallax plugin using pure javascript. Cross browser support, including mobile platforms. Based on goodparallax
 * @copyright © Marius Hansen 2018
 */
 
-var windowHeight = window.innerHeight,
-    windowHeightExtra = 0;
-var safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-    mobile = /Mobi/.test(navigator.userAgent);
+var windowHeight = window.innerHeight;
+if (/Mobi/.test(navigator.userAgent)) {
+	windowHeight = screen.height;
 
-// check if safari - extend height
-if (safari && !mobile) {
-	windowHeightExtra = window.outerHeight - window.innerHeight;
+	var mobileCss = document.createElement('style');
+	mobileCss.type = 'text/css';
+	mobileCss.innerHTML = '\n\t@media screen and (-webkit-min-device-pixel-ratio:0) {\n\t\t.parallax__container {\n\t\t\tclip: auto;\n\t\t\t-webkit-mask-image: -webkit-linear-gradient(top, #fff 0%, #fff 100%);\n\t\t\t-webkit-backface-visibility: hidden;\n\t\t\t-webkit-webkit-perspective: 1000;\n\t\t}\n\t};\n\t';
+
+	document.getElementsByTagName('head')[0].appendChild(mobileCss);
 }
 
-if (mobile) {
-	windowHeight = window.screen.availHeight; // stops from jumping
-	windowHeightExtra = (window.screen.availHeight - window.innerHeight) / 2; // prevents white spaces
+function calculateHeight(parallax, speed) {
+	for (var i = 0; parallax.length > i; i++) {
+		var container = parallax[i].parentElement.parentElement.getBoundingClientRect().height;
+
+		var elemOffsetTop = (windowHeight - container) / 2;
+		var bgHeight = Math.ceil(container + (elemOffsetTop - elemOffsetTop / speed) * 2) + 4;
+		parallax[i].style.height = bgHeight + 'px';
+	}
 }
 
-// position parallax
-function positionParallax(container, speed, parallax, elem) {
-	var bgScroll = container.top / speed - windowHeightExtra;
-	parallax[elem].style.top = bgScroll + 'px';
-}
-
-// animate parallax
 function animateParallax(parallax, speed) {
 	for (var i = 0; parallax.length > i; i++) {
 		var container = parallax[i].parentElement.parentElement.getBoundingClientRect();
 
 		if (container.top + container.height >= 0 && container.top <= windowHeight) {
-			positionParallax(container, speed, parallax, i);
+			var bgScroll = (container.top - 2) / speed;
+
+			parallax[i].style.top = bgScroll + 'px';
 		}
-	}
-}
-
-// determine height
-function calculateHeight(parallax, speed) {
-	for (var i = 0; parallax.length > i; i++) {
-		var container = parallax[i].parentElement.parentElement.getBoundingClientRect();
-		var elemOffsetTop = (windowHeight - container.height) / 2;
-		var bgHeight = container.height + (elemOffsetTop - elemOffsetTop / speed) * 2;
-
-		parallax[i].style.height = bgHeight + windowHeightExtra * 2 + 'px';
-		positionParallax(container, speed, parallax, i);
 	}
 }
 
 var universalParallax = function universalParallax() {
 	var up = function up(parallax, speed) {
-
-		// check that speed is not a negative number
 		if (speed < 1) {
 			speed = 1;
 		}
 
-		// set height on elements
 		calculateHeight(parallax, speed);
 
-		// recalculate height on resize
-		if (!mobile) {
-			window.addEventListener("resize", function () {
-				windowHeight = window.innerHeight;
-				calculateHeight(parallax, speed);
-			});
+		window.addEventListener("resize", function () {
+			calculateHeight(parallax, speed);
+		});
+
+		var latestKnownScrollY = 0,
+		    scrolled = false;
+
+		function onScroll() {
+			latestKnownScrollY = window.scrollY;
+			if (!scrolled) {
+				requestAnimationFrame(runScrollAnimations);
+			}
+			scrolled = true;
 		}
 
-		// Add scroll event listener
-		window.addEventListener("scroll", function () {
+		function runScrollAnimations() {
+			scrolled = false;
+			var currentScrollY = latestKnownScrollY;
 			animateParallax(parallax, speed);
-		});
+		}
+
+		window.addEventListener('scroll', onScroll, false);
 	};
 
-	// Ready all elements
 	this.init = function (param) {
 		if (typeof param === 'undefined') {
 			param = {};
@@ -90,23 +85,21 @@ var universalParallax = function universalParallax() {
 		var parallax = document.getElementsByClassName(param.className);
 
 		for (var i = 0; parallax.length > i; i++) {
-			// make container div
 			var wrapper = document.createElement('div');
 			parallax[i].parentNode.insertBefore(wrapper, parallax[i]);
 			wrapper.appendChild(parallax[i]);
 			var parallaxContainer = parallax[i].parentElement;
 			parallaxContainer.className += 'parallax__container';
 
-			// // parent elem need position: relative for effect to work - if not already defined, add it
 			if (window.getComputedStyle(parallaxContainer.parentElement, null).getPropertyValue('position') !== 'relative') {
 				parallaxContainer.parentElement.style.position = 'relative';
 			}
 
 			var imgData = parallax[i].dataset.parallaxImage;
-			// add image to div if none is specified
+
 			if (typeof imgData !== 'undefined') {
 				parallax[i].style.backgroundImage = 'url(' + imgData + ')';
-				// if no other class than .parallax is specified, add CSS
+
 				if (parallax[i].classList.length === 1 && parallax[i].classList[0] === 'parallax') {
 					Object.assign(parallax[i].style, {
 						'background-repeat': 'no-repeat',
@@ -117,7 +110,6 @@ var universalParallax = function universalParallax() {
 			}
 		};
 
-		// when init completed, run function
 		up(parallax, param.speed);
 	};
 };
