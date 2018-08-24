97a5773bb861c186138bd224783580bfd1fcddae
Marius Olai Hansen <Marius.Hansen@bringdialog.no>
2018-08-24 19:53:00 +0200

    updated new npm -v and upped version

--- dist/universal-parallax.js
+++ dist/universal-parallax.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
-* @version 1.2.0
+* @version 1.2.1
 * @author Marius Hansen <marius.o.hansen@gmail.com>
 * @license MIT
 * @description Easy parallax plugin using pure javascript. Cross browser support, including mobile platforms. Based on goodparallax
